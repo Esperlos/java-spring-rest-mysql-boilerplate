fc537225ab20763fddfdc87e35170c220459956f
Sajjad Asadi <lvlr.xaus@gmail.com>
2019-01-07 23:48:26 +0330

    init

---
+++ src/main/java/restsql/Car.java
@@ -0,0 +1,23 @@
+package restsql;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/restsql/CarRepository.java
@@ -0,0 +1,12 @@
+package restsql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import restsql.Car;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface CarRepository extends CrudRepository<Car, Integer> {
+
+}
